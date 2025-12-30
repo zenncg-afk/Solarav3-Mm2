007f856d46994bfeb8d682152956793b2783314b
zenncg-afk <zenncg@gmail.com>
2025-12-29 22:27:30 -0500

    add loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zenncg-afk/solarav3/main/init.lua"))()
+-- Loader for SolarA v3 MM2
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zenncg-afk/Solarav3-MM2/main/init.lua", true))()
