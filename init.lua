d792df9987a6e0caca898fb3bbf6590557307ed0
zenncg-afk <zenncg@gmail.com>
2025-12-29 21:30:19 -0500

    add init.lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local base = "https://raw.githubusercontent.com/YOURUSERNAME/solarav3/main/"
+
+local function Load(path)
+    return loadstring(game:HttpGet(base .. path))()
+end
+
+local Modules = {
+    Players = Load("modules/players.lua"),
+    Visuals = Load("modules/visuals.lua"),
+    Roles = Load("modules/roles.lua")
+}
+
+local UI = Load("ui.lua")
+UI:Init(Modules)
