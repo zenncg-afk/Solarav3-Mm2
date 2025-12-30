e1fc713e18cd45b85eabed5c3d84d46e31940b3d
zenncg-afk <zenncg@gmail.com>
2025-12-29 21:44:24 -0500

    add init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local base = "https://raw.githubusercontent.com/zenncg-afk/solarav3/main/"
+-- Make sure to replace YOURUSERNAME with your GitHub username
+local base = "https://raw.githubusercontent.com/zenncg-afk/Solarav3-Mm2/main/"
 
 local function Load(path)
     return loadstring(game:HttpGet(base .. path))()
